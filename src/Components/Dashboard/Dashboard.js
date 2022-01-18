530c3ab6ae1c337a6332bea5961585435de3f0c4
Anant Rawat <anant.rawat@sunfox.in>
2022-01-18 11:43:53 +0530

    Table added for Task Management in Dashboard

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,5 +1,42 @@
 import React from "react";
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            {/* <th scope="col">#</th> */}
+            <th scope="col">Backlog</th>
+            <th scope="col">To Do</th>
+            <th scope="col">On Going</th>
+            <th scope="col">Done</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {/* <th scope="row">1</th> */}
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>sdfgs</td>
+          </tr>
+          <tr>
+            {/* <th scope="row">2</th> */}
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            <td>asdf</td>
+          </tr>
+          <tr>
+            {/* <th scope="row">3</th> */}
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+            <td>asdf</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 export default Dashboard;
