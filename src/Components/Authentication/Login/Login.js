7e619a5f9f3ae8ea842aa1b371e9d75d996c6091
Anant Rawat <anant.rawat@sunfox.in>
2022-01-18 09:58:19 +0530

    Login action creater updated

--- src/Components/Authentication/Login/Login.js
+++ src/Components/Authentication/Login/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 import axios from "axios";
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -6,14 +7,14 @@ const Login = () => {
   const submitHandler = (event) => {
     event.preventDefault();
     // console.log(email, password);
-    axios
-      .get("http://localhost:3000/employees")
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    // axios
+    //   .get("http://localhost:3000/employees")
+    //   .then((res) => {
+    //     console.log(res.data);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
   };
   return (
     <div>
@@ -62,4 +63,8 @@ const Login = () => {
   );
 };
 
-export default Login;
+const mapStateToProps = (state) => {};
+
+const mapDispatchToProps = (dispatch) => {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- src/Redux/Authentication/AuthenticationActions.js
+++ src/Redux/Authentication/AuthenticationActions.js
@@ -2,17 +2,20 @@ import axios from "axios";
 import { LOGIN, REGISTER } from "./AuthenticationTypes";
 // import { REGISTER } from "./AuthenticationTypes";
 
-export const login = (data) => {
-  return {
-    type: LOGIN,
-    payload: data,
-  };
+export const login = (data) => async (dispatch) => {
+  const { email, password } = data;
+  try {
+    const res = await axios.get("http://localhost:3000/employees");
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const register = (data) => async (dispatch) => {
   const { name, userName, email, number, password } = data;
   // console.log(data);
-  console.log("Hello");
+  // console.log("Hello");
   try {
     const res = await axios.post("http://localhost:3000/employees", {
       name,
